a1ff9dadae90a59e02fe3ed854e01efd2e77c7f0
David <wisekrakr@gmail.com>
2018-02-02 03:41:01 +0100

    Every enemy has different kinds of modes Ammo now in hud A start screen is in place, not activated

--- core/src/com/wisekrakr/firstgame/Screens/StartScreen.java
+++ core/src/com/wisekrakr/firstgame/Screens/StartScreen.java
@@ -3,10 +3,18 @@ package com.wisekrakr.firstgame.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.wisekrakr.firstgame.Constants;
 import com.wisekrakr.firstgame.SpaceGameContainer;
 import com.wisekrakr.firstgame.client.ClientConnector;
@@ -16,29 +24,72 @@ import java.util.List;
 
 public class StartScreen extends ScreenAdapter {
 
-    private final ClientConnector connector;
-    private final List<String> players;
-    private final String mySelf;
-    private OrthographicCamera camera;
-    private PlayerPerspectiveScreen playerPerspectiveScreen;
-    private SpriteBatch batch;
+    private ClientConnector connector;
+    private List<String> players;
+    private String mySelf;
+    private Stage stage;
+    private Skin skin;
     private BitmapFont font;
     private SpaceGameContainer container;
 
-    public StartScreen(ClientConnector connector, List<String> players, String mySelf) {
+    public StartScreen(final ClientConnector connector, final List<String> players, final String mySelf) {
         this.connector = connector;
         this.players = players;
         this.mySelf = mySelf;
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
-
-        font = new BitmapFont(Gdx.files.internal("myFont.fnt"));
-        font.getData().setScale(0.8f);
-
-        batch = new SpriteBatch();
-
         container = new SpaceGameContainer();
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        //font = new BitmapFont(Gdx.files.internal("myFont.fnt"));
+        //font.getData().setScale(0.5f);
+
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        TextureAtlas textureAtlas = new TextureAtlas();
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+
+        //create buttons
+        TextButton newGame = new TextButton("New Game", skin);
+        TextButton preferences = new TextButton("Preferences", skin);
+        TextButton exit = new TextButton("Exit", skin);
+
+        //add buttons to table
+
+        table.add(newGame).fillX().uniformX();
+        table.row().pad(10, 0, 10, 0);
+        table.add(preferences).fillX().uniformX();
+        table.row();
+        table.add(exit).fillX().uniformX();
+
+        // create button listeners
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+        newGame.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                container.setScreen(new PlayerPerspectiveScreen(connector, players, mySelf));
+            }
+        });
+
+        preferences.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+            }
+        });
+
+
+
+
     }
 
     @Override
@@ -47,17 +98,8 @@ public class StartScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-
-        batch.begin();
-        font.draw(batch, "Don't Shoot my Spaceship!", 50, 300);
-        font.draw(batch, "Click to start", 50, 100);
-        batch.end();
-
-        if(Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)){
-            container.setScreen(new PlayerPerspectiveScreen(connector, players, mySelf));
-        }
+        stage.act();
+        stage.draw();
     }
 
     @Override
@@ -87,6 +129,7 @@ public class StartScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
-        super.dispose();
+
+        stage.dispose();
     }
 }
